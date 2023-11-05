a1a121f756287c07e9467613f3020c7ea5f4a4f1
Charles Chavis <chavisca@gmail.com>
2023-11-05 02:23:54 -0500

    correcting naming convention on portfolio.jsx

--- CCReactPortfolio/src/pages/Portfolio.jsx
+++ CCReactPortfolio/src/pages/Portfolio.jsx
@@ -1,11 +1,11 @@
-import Portfolio from '../data/portfolio';
+import PortfolioData from '../data/portfolio';
 import Portfolioitem from './Portfolioitem';
 
 export default function Portfolio() {
     return (
         <div className="flex flex-col md:flex-row items-center justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {Portfolio.map(project => (
+                {PortfolioData.map(project => (
                     <Portfolioitem 
                     imgUrl={project.imgUrl}
                     title={project.title}
